59abcc4a7a02325eef1d04e8ff03bc8ab3aaf449
Adley-Nastri <47566248+Adley-Nastri@users.noreply.github.com>
2019-12-13 20:01:41 +0000

    Update Paper.java

--- src/Paper.java
+++ src/Paper.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.stream.IntStream;
 
-public class Paper implements Comparable<Paper>{
+public class Paper implements Comparable<Paper>{ //Allows for the Paper objects to be compared
 	
 	String ID;
 	String title;
@@ -79,12 +79,13 @@ public class Paper implements Comparable<Paper>{
 
 	public double getAvg() {
 		
-		double sum = IntStream.of(this.scores).sum();
+		double sum = IntStream.of(this.scores).sum(); //Add up all the values in the score array, then convert into double format
 		return sum/this.scores.length;
 	}
 
 	public int getREF()
 	{
+		//TRY NOT TO CONFUSE WITH QUARTILES, in this case 4 is the better value, rather than 1
 		if (this.getAvg() >= 0 && this.getAvg() <1) {
 			this.REF = 0;
 		}
@@ -128,8 +129,8 @@ public class Paper implements Comparable<Paper>{
 
 	@Override
 	public int compareTo(Paper o) {
-		// TODO Auto-generated method stub
-		return Double.compare(o.getAvg(), this.getAvg());
+		return Double.compare(o.getAvg(), this.getAvg()); //Used by collections in order to compare and sort objects.
+		//In this case we are comparing and sorting based in the Paper's Avg value
 	
 	}
 
